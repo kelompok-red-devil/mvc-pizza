d6455f720c5a8f66797b6fd09a217be1645723b7
Lotsoo <braindeathrama@gmail.com>
2022-10-28 23:05:08 +0700

    Membuat controller kasir

---
+++ src/controller/ControllerKasir.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package controller;
+
+import java.sql.SQLException;
+import view.Kasir;
+
+/**
+ *
+ * @author lotso
+ */
+public interface ControllerKasir {
+    public void Simpan(Kasir kasir)throws SQLException;
+    public void Hapus(Kasir kasir)throws SQLException;
+    public void Tampil(Kasir kasir)throws SQLException;
+    public void KlikTable(Kasir kasir)throws SQLException;
+}
